fee83b13707bd1caf90f536438535a20b634cac9
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-10-05 12:17:32 +0300

    Add files via upload

---
+++ IfElseAndTernaryIf.cs
@@ -0,0 +1,29 @@
+﻿internal class IfElseAndTernaryIf
+{
+    private static void Main(string[] args)
+    {
+
+        // If - Else If 
+
+        int time = DateTime.Now.Hour;
+        Console.WriteLine(time);
+
+        if (time >= 6 && time < 12)
+        {
+            Console.WriteLine("Good morning!");
+        }else if (time <= 16) {
+            Console.WriteLine("Have a good day!");
+        }else if (time < 20) {
+            Console.WriteLine("Good evening!");
+        }else {
+            Console.WriteLine("Good night!");
+        }
+
+        // Ternary If
+
+        string result = time<=18 ? "Good day":"Good night";
+        result = time >= 6 && time < 12 ? "Good morning!" : time <= 16 ? "Have a good day!" : "Good night!";
+        Console.WriteLine(result);
+
+    }
+}
