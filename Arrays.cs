addae7331ff2d2a6e8c8aef1e07bcd101ba06b14
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-10-05 21:32:14 +0300

    Add files via upload

---
+++ Arrays.cs
@@ -0,0 +1,42 @@
+﻿internal class Arrays
+{
+    private static void Main(string[] args)
+    {
+        // Dizi Tanımla
+        string[] colors = new string[5];
+
+        string[] animals = { "cat", "dog", "bird", "monkey" };
+
+        int[] list;
+        list = new int[5];
+
+        // Dizilere Değer Atama ve Erişim
+        colors[0] = "Blue";
+        list[3] = 10;
+
+        Console.WriteLine(animals[1]);
+        Console.WriteLine(list[3]);
+        Console.WriteLine(colors[0]);
+
+        // Döngülerle dizi kullanımı
+
+        Console.Write("Please, enter a number of elements: ");
+        int arrayLenght = Convert.ToInt32(Console.ReadLine());
+
+        int[] numberList = new int[arrayLenght];
+
+        for (int i = 0; i < arrayLenght; i++)
+        {
+            Console.Write("Please enter {0}. number: ", i + 1);
+            numberList[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int total = 0;
+
+        foreach (var number in numberList)
+        {
+            total += number;
+        }
+        Console.WriteLine("Average: " + total / arrayLenght);
+    }
+}
