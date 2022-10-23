fadfc98198b0f4f9c43c3bbc02cd6297cf682065
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-10-23 22:24:21 +0300

    Add files via upload

---
+++ Easy/Algorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string: ");
+        string w = Console.ReadLine()!;
+        Console.WriteLine("Enter an index value: ");
+        int i = int.Parse(Console.ReadLine()!);
+
+        updateWord(w, i);
+        
+    }
+    public static void updateWord(string word, int index)
+    {
+        if (index > word.Length || index < 0)
+        {
+            Console.WriteLine(word);
+        }
+        else
+        {
+            string updatedWord = word.Remove(index, 1);
+            Console.WriteLine(updatedWord);
+        }
+    }
+}
