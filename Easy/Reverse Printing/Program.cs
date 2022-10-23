cdc08700e552a515dab46e8bbcc4d4e65613cf30
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-10-23 23:54:42 +0300

    Add files via upload

---
+++ Easy/Reverse Printing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sentence: ");
+        string str = Console.ReadLine()!;
+
+        firstChar(str);
+
+    }
+    static void firstChar(string sentence)
+    {
+        string[] arr = sentence.Split();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            string str = arr[i];
+            string firstChar = str.Substring(0, 1);
+            string restOfWord = str.Remove(0, 1);
+            Console.Write(String.Concat(restOfWord, firstChar));
+            Console.Write(" ");
+        }
+    }
+}
