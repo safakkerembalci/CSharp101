d01f62960367d89742c5414d36e6f18876a79dec
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-10-26 13:21:13 +0300

    Add files via upload

---
+++ Easy/Draw Circle/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        double radius;
+        double thickness = 0.4;
+
+        Console.Write("Enter radius: ");
+        if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+        {
+            Console.WriteLine("Radius cannot be negative value!");
+            return;
+        }
+
+        while (radius <= 0) ;
+
+        Console.WriteLine();
+        double rin = radius - thickness, rOut = radius + thickness;
+
+        for (double y = radius; y >= -radius; --y)
+        {
+            for (double x = -radius; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= rin * rin && value <= rOut * rOut)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
