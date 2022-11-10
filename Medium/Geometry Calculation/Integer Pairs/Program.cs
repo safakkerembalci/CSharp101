ba7a859b2d72e96eea05e1650322f2d81e3e4f60
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-11-10 16:03:16 +0300

    Add files via upload

---
+++ Medium/Geometry Calculation/Integer Pairs/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Write("How many integers will you enter: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[] numberList = new int[n];
+
+if (n % 2 == 0)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Enter "+ (i + 1) + ". number: ");
+        numberList[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    for (int i = 0; i < numberList.Length; i += 2)
+    {
+        if (numberList[i] == numberList[i + 1])
+        {
+            int total = numberList[i] + numberList[i + 1];
+            Console.Write(total*total + " ");
+        }
+        else
+        {
+            int total = numberList[i] + numberList[i + 1];
+            Console.Write(total + " ");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Please enter an even number!");
+}
+
+
+
+
