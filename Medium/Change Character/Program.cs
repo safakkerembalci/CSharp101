60418a651ce3c517dad4e6767b301310e23319f7
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-11-10 20:01:47 +0300

    Add files via upload

---
+++ Medium/Change Character/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Please enter a sentence: ");
+string sentence = Console.ReadLine()!;
+string[] words = sentence.Split();
+
+foreach (var chr in words)
+{
+    char[] chars = chr.ToCharArray();
+    char firstChar = chars[0];
+    chars[0] = chars[chars.Length - 1];
+    chars[chars.Length - 1] = firstChar;
+
+    foreach (var chr1 in chars)
+    {
+        string newSentence = chr1.ToString();
+        Console.Write(newSentence);
+    }
+    Console.Write(" ");
+}
+
+
