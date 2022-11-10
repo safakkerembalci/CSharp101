98f8724de1d1e684ced8fe3cd133ab63a85f657b
Şafak Kerem Balcı <104922046+safakkerembalci@users.noreply.github.com>
2022-11-10 21:43:21 +0300

    Add files via upload

---
+++ Medium/Consonant/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Enter a sentence: ");
+string sentence = Console.ReadLine()!;
+string[] words = sentence.Split();
+char[] vovels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü', 'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü' };
+
+for (int i = 0; i < words.Length; i++)
+{
+    bool value = false;
+    char[] chars = words[i].ToCharArray();
+
+    for (int j = 0; j < chars.Length; j++)
+    {
+        if (!vovels.Contains(chars[j]) && j != chars.Length - 1 && !vovels.Contains(chars[j + 1]))
+        {
+            value = true;
+            break;
+        }
+    }
+    Console.Write(value + " ");
+}
+
+
+
